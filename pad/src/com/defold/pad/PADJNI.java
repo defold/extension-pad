cf525b457c4106cf2a7993cbd5cc7e0835ba2912
Björn Ritzl <bjorn.ritzl@gmail.com>
2025-10-02 16:57:56 +0200

    Initial commit

---
+++ pad/src/com/defold/pad/PADJNI.java
@@ -0,0 +1,239 @@
+package com.defold.pad;
+
+import androidx.annotation.NonNull;
+
+import android.content.Context;
+import android.app.Activity;
+import android.util.Log;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnFailureListener;
+
+import com.google.android.play.core.assetpacks.AssetPackManagerFactory;
+import com.google.android.play.core.assetpacks.AssetPackManager;
+import com.google.android.play.core.assetpacks.AssetPackStates;
+import com.google.android.play.core.assetpacks.AssetPackState;
+import com.google.android.play.core.assetpacks.AssetPackLocation;
+import com.google.android.play.core.assetpacks.model.AssetPackErrorCode;
+import com.google.android.play.core.assetpacks.model.AssetPackStatus;
+
+
+public class PADJNI {
+    private static final String TAG = "PADJNI";
+
+    private enum PADEventType {
+        PACK_STATE_UPDATED,
+        PACK_STATE_ERROR,
+        
+        REMOVE_PACK_COMPLETED,
+        REMOVE_PACK_CANCELED,
+        REMOVE_PACK_ERROR,
+
+        DIALOG_CONFIRMED,
+        DIALOG_DECLINED,
+        DIALOG_CANCELED,
+        DIALOG_ERROR,
+    }
+
+    private static class PADEvent {
+        public String packName;
+        public PADEventType eventType;
+        public String extra;
+
+        public PADEvent(String packName, PADEventType eventType) {
+            this.packName = packName;
+            this.eventType = eventType;
+        }
+    }
+
+    private Activity activity;
+    private AssetPackManager assetPackManager;
+
+    private Map<String, AssetPackState> stateCache = new ConcurrentHashMap<>();
+
+    private List<PADEvent> events = Collections.synchronizedList(new ArrayList<>());
+
+    public PADJNI(Activity activity) {
+        this.activity = activity;
+        Log.d(TAG, "Constructor");
+        assetPackManager = AssetPackManagerFactory.getInstance(activity);
+    }
+
+    private void AddEvent(String packName, PADEventType eventType) {
+        PADEvent event = new PADEvent(packName, eventType);
+        synchronized (events) {
+            events.add(event);
+        }
+    }
+    private void AddEvent(String packName, PADEventType eventType, String extra) {
+        PADEvent event = new PADEvent(packName, eventType);
+        event.extra = extra;
+        synchronized (events) {
+            events.add(event);
+        }
+    }
+
+    public String GetNextEvent() {
+        PADEvent event = null;
+        synchronized (events) {
+            if (!events.isEmpty()) {
+                event = events.remove(0);
+            }
+        }
+
+        JSONObject jo = new JSONObject();
+        if (event != null) {
+            try {
+                jo.put("pack_name", event.packName);
+                jo.put("event_type", event.eventType.ordinal());
+                jo.put("extra", event.extra);
+            }
+            catch (JSONException e) {
+                Log.e(TAG, "JSON object creation error: " + e.getMessage());
+            }
+        }
+        return jo.toString();
+    }
+
+    public void Cancel(String packName) {
+        Log.d(TAG, "Cancel");
+        List<String> packNames = Arrays.asList(packName);
+        AssetPackStates states = assetPackManager.cancel(packNames);
+        AssetPackState state = states.packStates().get(packName);
+        stateCache.put(packName, state);
+        AddEvent(packName, PADEventType.PACK_STATE_UPDATED);
+    }
+
+    public void Fetch(String packName) {
+        Log.d(TAG, "Fetch " + packName);
+        List<String> packNames = Arrays.asList(packName);
+        assetPackManager.fetch(packNames);
+    }
+
+    public boolean HasPackState(String packName) {
+        Log.d(TAG, "HasPackState " + packName);
+        return stateCache.containsKey(packName);
+    }
+
+
+    public void GetPackState(String packName) {
+        Log.d(TAG, "GetPackState " + packName);
+        List<String> packNames = Arrays.asList(packName);
+        Task<AssetPackStates> task = assetPackManager.getPackStates(packNames);
+        task.addOnCompleteListener(new OnCompleteListener<AssetPackStates>() {
+            @Override
+            public void onComplete(@NonNull Task<AssetPackStates> task) {
+                AssetPackStates states = task.getResult();
+                AssetPackState state = states.packStates().get(packName);
+                stateCache.put(packName, state);
+                AddEvent(packName, PADEventType.PACK_STATE_UPDATED);
+            }
+        });
+        task.addOnCanceledListener(new OnCanceledListener() {
+            @Override
+            public void onCanceled() {
+                AddEvent(packName, PADEventType.PACK_STATE_ERROR);
+            }
+        });
+        task.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(Exception e) {
+                AddEvent(packName, PADEventType.PACK_STATE_ERROR, e.getMessage());
+            }
+        });
+    }
+
+    public long GetPackBytesDownloaded(String packName) {
+        Log.d(TAG, "GetPackBytesDownloaded " + packName);
+        AssetPackState state = stateCache.get(packName);
+        return (state != null) ? state.bytesDownloaded() : 0;
+    }
+    public int GetPackErrorCode(String packName) {
+        Log.d(TAG, "GetPackErrorCode " + packName);
+        AssetPackState state = stateCache.get(packName);
+        return (state != null) ? state.errorCode() : AssetPackErrorCode.PACK_UNAVAILABLE;
+    }
+    public int GetPackStatus(String packName) {
+        Log.d(TAG, "GetPackStatus " + packName);
+        AssetPackState state = stateCache.get(packName);
+        return (state != null) ? state.status() : AssetPackStatus.UNKNOWN;
+    }
+    public long GetPackTotalBytesToDownload(String packName) {
+        Log.d(TAG, "GetPackTotalBytesToDownload " + packName);
+        AssetPackState state = stateCache.get(packName);
+        return (state != null) ? state.totalBytesToDownload() : 0;
+    }
+    public int GetPackTransferProgressPercentage(String packName) {
+        Log.d(TAG, "GetPackTransferProgressPercentage " + packName);
+        AssetPackState state = stateCache.get(packName);
+        return (state != null) ? state.transferProgressPercentage() : 0;
+    }
+    public String GetPackLocation(String packName) {
+        Log.d(TAG, "GetPackLocation " + packName);
+        AssetPackLocation location = assetPackManager.getPackLocation(packName);
+        String path = location.assetsPath();
+        Log.d(TAG, "GetPackLocation path = " + path);
+        return path;
+    }
+
+    public void RemovePack(String packName) {
+        Log.d(TAG, "RemovePack " + packName);
+        Task<Void> task = assetPackManager.removePack(packName);
+        task.addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                AddEvent(packName, PADEventType.REMOVE_PACK_COMPLETED);
+            }
+        });
+        task.addOnCanceledListener(new OnCanceledListener() {
+            @Override
+            public void onCanceled() {
+                AddEvent(packName, PADEventType.REMOVE_PACK_CANCELED);
+            }
+        });
+        task.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(Exception e) {
+                AddEvent(packName, PADEventType.REMOVE_PACK_ERROR, e.getMessage());
+            }
+        });
+    }
+
+    public void ShowConfirmationDialog(String packName) {
+        Log.d(TAG, "ShowConfirmationDialog " + packName);
+        // Task<Integer> task = assetPackManager.showConfirmationDialog(activity);
+        // task.addOnCompleteListener(new OnCompleteListener<Integer>() {
+        //     @Override
+        //     public void onComplete(@NonNull Task<Integer> task) {
+        //         PADEventType eventType = (task.getResult() == Activity.RESULT_OK) ? PADEventType.DIALOG_CONFIRMED : PADEventType.DIALOG_DECLINED;
+        //         AddEvent(packName, eventType);
+        //     }
+        // });
+        // task.addOnCanceledListener(new OnCanceledListener() {
+        //     @Override
+        //     public void onCanceled() {
+        //         AddEvent(packName, PADEventType.DIALOG_CANCELED);
+        //     }
+        // });
+        // task.addOnFailureListener(new OnFailureListener() {
+        //     @Override
+        //     public void onFailure(Exception e) {
+        //         AddEvent(packName, PADEventType.DIALOG_ERROR, e.getMessage());
+        //     }
+        // });
+
+    }
+}
---
+++ pad/src/pad.cpp
@@ -0,0 +1,485 @@
+#define LIB_NAME "PAD"
+#define MODULE_NAME "pad"
+
+#include <dmsdk/sdk.h>
+
+#if defined(DM_PLATFORM_ANDROID)
+
+#include <dmsdk/dlib/android.h>
+
+struct PADJNI
+{
+    jobject        m_JNI;
+    jmethodID      m_Cancel;
+    jmethodID      m_Fetch;
+    jmethodID      m_HasPackState;
+    jmethodID      m_GetPackState;
+    jmethodID      m_RemovePack;
+    jmethodID      m_ShowConfirmationDialog;
+
+    jmethodID      m_GetNextEvent;
+
+    jmethodID      m_GetPackLocation;
+    jmethodID      m_GetPackBytesDownloaded;
+    jmethodID      m_GetPackErrorCode;
+    jmethodID      m_GetPackStatus;
+    jmethodID      m_GetPackTotalBytesToDownload;
+    jmethodID      m_GetPackTransferProgressPercentage;
+
+};
+
+static PADJNI g_PAD;
+static dmScript::LuaCallbackInfo* g_PADListener;
+
+
+static void CallVoidMethod(jobject instance, jmethodID method)
+{
+    dmAndroid::ThreadAttacher threadAttacher;
+    JNIEnv* env = threadAttacher.GetEnv();
+    env->CallVoidMethod(instance, method);
+}
+
+static void CallVoidMethodLong(jobject instance, jmethodID method, const uint64_t arg1)
+{
+    dmAndroid::ThreadAttacher threadAttacher;
+    JNIEnv* env = threadAttacher.GetEnv();
+    env->CallVoidMethod(instance, method, arg1);
+}
+
+static int CallIntMethodVoid(jobject instance, jmethodID method)
+{
+    dmAndroid::ThreadAttacher threadAttacher;
+    JNIEnv* env = threadAttacher.GetEnv();
+    jint result = env->CallIntMethod(instance, method);
+    return result;
+}
+
+static float CallFloatMethodInt(jobject instance, jmethodID method, const int arg1)
+{
+    dmAndroid::ThreadAttacher threadAttacher;
+    JNIEnv* env = threadAttacher.GetEnv();
+    jfloat result = env->CallFloatMethod(instance, method, arg1);
+    return result;
+}
+
+
+static int CallIntMethodChar(jobject instance, jmethodID method, const char* cstr)
+{
+    dmAndroid::ThreadAttacher threadAttacher;
+    JNIEnv* env = threadAttacher.GetEnv();
+
+    jstring jstr = env->NewStringUTF(cstr);
+    jint result = env->CallIntMethod(instance, method, jstr);
+    env->DeleteLocalRef(jstr);
+    return result;
+}
+
+static long CallLongMethodChar(jobject instance, jmethodID method, const char* cstr)
+{
+    dmAndroid::ThreadAttacher threadAttacher;
+    JNIEnv* env = threadAttacher.GetEnv();
+
+    jstring jstr = env->NewStringUTF(cstr);
+    jlong result = env->CallLongMethod(instance, method, jstr);
+    env->DeleteLocalRef(jstr);
+    return result;
+}
+
+
+static void CallVoidMethodChar(jobject instance, jmethodID method, const char* cstr)
+{
+    dmAndroid::ThreadAttacher threadAttacher;
+    JNIEnv* env = threadAttacher.GetEnv();
+
+    jstring jstr = env->NewStringUTF(cstr);
+    env->CallVoidMethod(instance, method, jstr);
+    env->DeleteLocalRef(jstr);
+}
+
+static char* CallCharMethodChar(jobject instance, jmethodID method, const char* cstr)
+{
+    dmAndroid::ThreadAttacher threadAttacher;
+    JNIEnv* env = threadAttacher.GetEnv();
+
+    jstring jstr = env->NewStringUTF(cstr);
+    jstring return_value = (jstring)env->CallObjectMethod(instance, method, jstr);
+    env->DeleteLocalRef(jstr);
+
+    const char* result_cstr = env->GetStringUTFChars(return_value, 0);
+    char* result_cstr_copy = strdup(result_cstr);
+    env->ReleaseStringUTFChars(return_value, result_cstr);
+    env->DeleteLocalRef(return_value);
+
+    return result_cstr_copy;
+}
+
+static char* CallCharMethodVoid(jobject instance, jmethodID method)
+{
+    dmAndroid::ThreadAttacher threadAttacher;
+    JNIEnv* env = threadAttacher.GetEnv();
+
+    jstring return_value = (jstring)env->CallObjectMethod(instance, method);
+
+    const char* result_cstr = env->GetStringUTFChars(return_value, 0);
+    char* result_cstr_copy = strdup(result_cstr);
+    env->ReleaseStringUTFChars(return_value, result_cstr);
+    env->DeleteLocalRef(return_value);
+
+    return result_cstr_copy;
+}
+
+static void HandleEvent(const char* event)
+{
+    if (!g_PADListener)
+    {
+        dmLogWarning("PAD callback is not set");
+        return;
+    }
+
+    if (!dmScript::IsCallbackValid(g_PADListener))
+    {
+        dmLogError("PAD callback is not valid");
+        g_PADListener = 0;
+        return;
+    }
+
+    if (!dmScript::SetupCallback(g_PADListener))
+    {
+        dmLogError("PAD callback setup failed");
+        dmScript::DestroyCallback(g_PADListener);
+        g_PADListener = 0;
+        return;
+    }
+
+    lua_State* L = dmScript::GetCallbackLuaContext(g_PADListener);
+
+    dmScript::JsonToLua(L, event, strlen(event));
+
+    // self, event
+    int ret = lua_pcall(L, 2, 0, 0);
+    if (ret != 0)
+    {
+        dmLogError("Error invoking PAD listener %d (LUA_ERRRUN = %d LUA_ERRMEM = %d LUA_ERRERR = %d)", ret, LUA_ERRRUN, LUA_ERRMEM, LUA_ERRERR);
+        lua_pop(L, 1);
+    }
+    dmScript::TeardownCallback(g_PADListener);
+}
+
+/**
+ * Requests to cancel the download of the specified asset packs.
+ * @name cancel
+ * @string pack_name
+ */
+static int PADCancel(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 0);
+    const char* pack_name = luaL_checkstring(L, 1);
+    CallVoidMethodChar(g_PAD.m_JNI, g_PAD.m_Cancel, pack_name);
+    return 0;
+}
+
+/**
+ * Requests to download the specified asset pack.
+ * @name fetch
+ * @string pack_name
+ */
+static int PADFetch(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 0);
+    const char* pack_name = luaL_checkstring(L, 1);
+    CallVoidMethodChar(g_PAD.m_JNI, g_PAD.m_Fetch, pack_name);
+    return 0;
+}
+
+/**
+ * Deletes the specified asset pack from the internal storage of the app.
+ * @name remove_pack
+ * @string pack_name
+ */
+static int PADRemovePack(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 0);
+    const char* pack_name = luaL_checkstring(L, 1);
+    CallVoidMethodChar(g_PAD.m_JNI, g_PAD.m_RemovePack, pack_name);
+    return 0;
+}
+
+/**
+ * Requests download state and details for the specified asset pack. This is an
+ * asynchronous function and will send a `PACK_STATE_UPDATED` event once the new
+ * state is available.
+ * @name get_pack_state
+ * @string pack_name
+ */
+static int PADGetPackState(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 0);
+    const char* pack_name = luaL_checkstring(L, 1);
+    CallVoidMethodChar(g_PAD.m_JNI, g_PAD.m_GetPackState, pack_name);
+    return 0;
+}
+
+/**
+ * Returns the location of the specified asset pack on the device or an empty
+ * string if this pack is not downloaded.
+ * @name get_pack_transfer_progress_percentage
+ * @string pack_name
+ * @treturn string
+ */
+static int PADGetPackLocation(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+    const char* pack_name = luaL_checkstring(L, 1);
+    char* location = CallCharMethodChar(g_PAD.m_JNI, g_PAD.m_GetPackLocation, pack_name);
+    lua_pushstring(L, location);
+    free(location);
+    return 1;
+}
+
+/**
+ * Returns the total number of bytes already downloaded for the pack.
+ * NOTE: You must have called the `get_pack_state()` function first and wait for
+ * a `PACK_STATE_UPDATED` event before calling this function.
+ * @name get_pack_bytes_downloaded
+ * @string pack_name
+ * @treturn bytes_downloaded
+ */
+static int PADGetPackBytesDownloaded(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+    const char* pack_name = luaL_checkstring(L, 1);
+    long bytes_downloaded = CallLongMethodChar(g_PAD.m_JNI, g_PAD.m_GetPackBytesDownloaded, pack_name);
+    lua_pushnumber(L, bytes_downloaded);
+    return 1;
+}
+
+/**
+ * Returns the error code for the pack, if Play has failed to download the pack.
+ * NOTE: You must have called the `get_pack_state()` function first and wait for
+ * a `PACK_STATE_UPDATED` event before calling this function.
+ * @name get_pack_error_code
+ * @string pack_name
+ * @treturn error_code
+ */
+static int PADGetPackErrorCode(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+    const char* pack_name = luaL_checkstring(L, 1);
+    int error_code = CallIntMethodChar(g_PAD.m_JNI, g_PAD.m_GetPackErrorCode, pack_name);
+    lua_pushnumber(L, error_code);
+    return 1;
+}
+
+/**
+ * Returns the download status of the pack.
+ * NOTE: You must have called the `get_pack_state()` function first and wait for
+ * a `PACK_STATE_UPDATED` event before calling this function.
+ * @name get_pack_status
+ * @string pack_name
+ * @treturn status
+ */
+static int PADGetPackStatus(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+    const char* pack_name = luaL_checkstring(L, 1);
+    int pack_status = CallIntMethodChar(g_PAD.m_JNI, g_PAD.m_GetPackStatus, pack_name);
+    lua_pushnumber(L, pack_status);
+    return 1;
+}
+
+/**
+ * Returns the total size of the pack in bytes.
+ * NOTE: You must have called the `get_pack_state()` function first and wait for
+ * a `PACK_STATE_UPDATED` event before calling this function.
+ * @name get_pack_total_bytes_to_download
+ * @string pack_name
+ * @treturn bytes_to_download
+ */
+static int PADGetPackTotalBytesToDownload(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+    const char* pack_name = luaL_checkstring(L, 1);
+    long bytes_to_download = CallLongMethodChar(g_PAD.m_JNI, g_PAD.m_GetPackTotalBytesToDownload, pack_name);
+    lua_pushnumber(L, bytes_to_download);
+    return 1;
+}
+
+/**
+ * Returns the percentage of the asset pack already transferred to the app.
+ * NOTE: You must have called the `get_pack_state()` function first and wait for
+ * a `PACK_STATE_UPDATED` event before calling this function.
+ * @name get_pack_transfer_progress_percentage
+ * @string pack_name
+ * @treturn number
+ */
+static int PADGetPackTransferProgressPercentage(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+    const char* pack_name = luaL_checkstring(L, 1);
+    int percentage = CallIntMethodChar(g_PAD.m_JNI, g_PAD.m_GetPackTransferProgressPercentage, pack_name);
+    lua_pushnumber(L, percentage);
+    return 1;
+}
+
+/** Shows a dialog that asks the user for consent to download packs
+ * Shows a dialog that asks the user for consent to download packs that are
+ * currently in either the AssetPackStatus.REQUIRES_USER_CONFIRMATION state or
+ * the AssetPackStatus.WAITING_FOR_WIFI state. Will return result in event
+ * listener.
+ * @name show_confirmation_dialog
+ * @string pack_name
+ */
+static int PADShowConfirmationDialog(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 0);
+    const char* pack_name = luaL_checkstring(L, 1);
+    CallVoidMethodChar(g_PAD.m_JNI, g_PAD.m_ShowConfirmationDialog, pack_name);
+    return 0;
+}
+
+/**
+ * Set listener to receive events
+ * @name set_listener
+ * @function listener The function to call (self, event)
+ */
+static int PADSetListener(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 0);
+    if (g_PADListener)
+    {
+        dmScript::DestroyCallback(g_PADListener);
+        g_PADListener = 0;
+    }
+    g_PADListener = dmScript::CreateCallback(L, 1);
+    return 0;
+}
+
+// Functions exposed to Lua
+static const luaL_reg Module_methods[] =
+{
+    {"set_listener", PADSetListener},
+    {"cancel", PADCancel},
+    {"fetch", PADFetch},
+    {"get_pack_state", PADGetPackState},
+    {"get_pack_location", PADGetPackLocation},
+    {"get_pack_bytes_downloaded", PADGetPackBytesDownloaded},
+    {"get_pack_error_code", PADGetPackErrorCode},
+    {"get_pack_status", PADGetPackStatus},
+    {"get_pack_total_bytes_to_download", PADGetPackTotalBytesToDownload},
+    {"get_pack_transfer_progress_percentage", PADGetPackTransferProgressPercentage},
+    {"remove_pack", PADRemovePack},
+    {"show_confirmation_dialog", PADShowConfirmationDialog},
+    {0, 0}
+};
+
+static void LuaInit(lua_State* L)
+{
+    int top = lua_gettop(L);
+    luaL_register(L, MODULE_NAME, Module_methods);
+    lua_pop(L, 1);
+    assert(top == lua_gettop(L));
+}
+
+
+static void JavaInit() {
+    dmAndroid::ThreadAttacher threadAttacher;
+    JNIEnv* env = threadAttacher.GetEnv();
+    jclass cls = dmAndroid::LoadClass(env, "com.defold.adpf.ADPFJNI");
+
+    jmethodID jni_constructor = env->GetMethodID(cls, "<init>", "(Landroid/app/Activity;)V");
+    g_PAD.m_JNI = env->NewGlobalRef(env->NewObject(cls, jni_constructor, threadAttacher.GetActivity()->clazz));
+
+    g_PAD.m_Cancel = env->GetMethodID(cls, "Cancel", "(Ljava/lang/String;)V");
+    g_PAD.m_Fetch = env->GetMethodID(cls, "Fetch", "(Ljava/lang/String;)V");
+    g_PAD.m_GetPackState = env->GetMethodID(cls, "GetPackState", "(Ljava/lang/String;)V");
+    g_PAD.m_HasPackState = env->GetMethodID(cls, "HasPackState", "(Ljava/lang/String;)Z");
+    g_PAD.m_RemovePack = env->GetMethodID(cls, "RemovePack", "(Ljava/lang/String;)V");
+    g_PAD.m_ShowConfirmationDialog = env->GetMethodID(cls, "ShowConfirmationDialog", "(Ljava/lang/String;)V");
+
+    g_PAD.m_GetNextEvent = env->GetMethodID(cls, "GetNextEvent", "(V)Ljava/lang/String;");
+
+    g_PAD.m_GetNextEvent = env->GetMethodID(cls, "GetNextEvent", "(V)Ljava/lang/String;");
+    g_PAD.m_GetPackBytesDownloaded = env->GetMethodID(cls, "GetPackBytesDownloaded", "(Ljava/lang/String;)J");
+    g_PAD.m_GetPackErrorCode = env->GetMethodID(cls, "GetPackErrorCode", "(Ljava/lang/String;)I");
+    g_PAD.m_GetPackStatus = env->GetMethodID(cls, "GetPackStatus", "(Ljava/lang/String;)I");
+    g_PAD.m_GetPackTotalBytesToDownload = env->GetMethodID(cls, "GetPackTotalBytesToDownload", "(Ljava/lang/String;)J");
+    g_PAD.m_GetPackTransferProgressPercentage = env->GetMethodID(cls, "GetPackTransferProgressPercentage", "(Ljava/lang/String;)I");
+    g_PAD.m_GetPackLocation = env->GetMethodID(cls, "GetPackLocation", "(Ljava/lang/String;)Ljava/lang/String;");
+}
+
+
+#endif
+
+
+dmExtension::Result AppInitializePAD(dmExtension::AppParams* params)
+{
+    dmLogInfo("AppInitializePAD\n");
+    return dmExtension::RESULT_OK;
+}
+
+dmExtension::Result InitializePAD(dmExtension::Params* params)
+{
+#if !defined(DM_PLATFORM_ANDROID)
+    dmLogInfo("Extension %s is not supported", LIB_NAME);
+#else
+    dmLogInfo("Registered %s Extension\n", MODULE_NAME);
+    LuaInit(params->m_L);
+    JavaInit();
+#endif
+    return dmExtension::RESULT_OK;
+}
+
+dmExtension::Result AppFinalizePAD(dmExtension::AppParams* params)
+{
+    dmLogInfo("AppFinalizePAD\n");
+    return dmExtension::RESULT_OK;
+}
+
+dmExtension::Result FinalizePAD(dmExtension::Params* params)
+{
+    dmLogInfo("FinalizePAD");
+#if defined(DM_PLATFORM_ANDROID)
+    if (g_PADListener)
+    {
+        dmScript::DestroyCallback(g_PADListener);
+        g_PADListener = 0;
+    }
+#endif
+    return dmExtension::RESULT_OK;
+}
+
+dmExtension::Result OnUpdatePAD(dmExtension::Params* params)
+{
+#if defined(DM_PLATFORM_ANDROID)
+    while (const char* event_json = CallCharMethodVoid(g_PAD.m_JNI, g_PAD.m_GetNextEvent))
+    {
+        dmLogInfo("OnUpdatePAD event %s", event_json);
+        HandleEvent(event_json);
+    }
+#endif
+    return dmExtension::RESULT_OK;
+}
+
+void OnEventPAD(dmExtension::Params* params, const dmExtension::Event* event)
+{
+    switch(event->m_Event)
+    {
+        case dmExtension::EVENT_ID_ACTIVATEAPP:
+            dmLogInfo("OnEventPAD - EVENT_ID_ACTIVATEAPP\n");
+            break;
+        case dmExtension::EVENT_ID_DEACTIVATEAPP:
+            dmLogInfo("OnEventPAD - EVENT_ID_DEACTIVATEAPP\n");
+            break;
+        case dmExtension::EVENT_ID_ICONIFYAPP:
+            dmLogInfo("OnEventPAD - EVENT_ID_ICONIFYAPP\n");
+            break;
+        case dmExtension::EVENT_ID_DEICONIFYAPP:
+            dmLogInfo("OnEventPAD - EVENT_ID_DEICONIFYAPP\n");
+            break;
+        default:
+            dmLogWarning("OnEventPAD - Unknown event id\n");
+            break;
+    }
+}
+
+DM_DECLARE_EXTENSION(PAD, LIB_NAME, AppInitializePAD, AppFinalizePAD, InitializePAD, OnUpdatePAD, OnEventPAD, FinalizePAD)
